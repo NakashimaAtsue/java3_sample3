1a98902e9feb2fa1226ff0322e060463279d621d
kizu-kana <2741005@bemax.jp>
2016-07-25 12:27:27 +0900

    Lion

---
+++ src/Lion.java
@@ -0,0 +1,29 @@
+package animal;
+
+public class Lion implements Animal{
+	
+        private String name = "ライオン";
+	private int speed = 60;
+	public String getName(){
+
+		return name;
+	}
+
+	public int getSpeed(){
+
+		return speed ;
+	}
+
+	public String race(Animal a){
+
+		if( getSpeed() == a.getSpeed() ){
+		return "draw" ;
+
+		}else if( getSpeed() < a.getSpeed() ){
+		return  "ライオン lost";
+
+		}else{
+		return "ライオン　lost" ;
+		}
+	}
+}
